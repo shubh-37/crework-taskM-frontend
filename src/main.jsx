c14284ab8e92c26f720b75cca5ea18a67095b580
Shubh Arya <shubh.arya37@gmail.com>
2024-07-31 22:54:10 +0530

    wrapped App with context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AuthProvider from './context/AuthContextProvider.jsx';
+import TaskContextProvider from './context/TaskContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <TaskContextProvider>
+          <App />
+        </TaskContextProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
