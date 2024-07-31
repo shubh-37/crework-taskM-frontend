b2ef43424d8259713623691cf9f4d6ff552ccae2
Shubh Arya <shubh.arya37@gmail.com>
2024-07-31 18:30:45 +0530

    dragndrop feat, create task modal and styled navbar

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,32 +1,94 @@
 import { NavLink } from 'react-router-dom';
 import '../css/landing.css';
+import bellIcon from '../assets/icons/bell.svg';
+import lightIcon from '../assets/icons/light.svg';
+import forwardIcon from '../assets/icons/forward.svg';
+import homeIcon from '../assets/icons/home.svg';
+import settingIcon from '../assets/icons/setting.svg';
+import boardIcon from '../assets/icons/board.svg';
+import teamsIcon from '../assets/icons/teams.svg';
+import analyticsIcon from '../assets/icons/analytics.svg';
+import plusIcon from '../assets/icons/plus.svg';
+import userIcon from '../assets/icons/user.svg';
+import TaskCard from '../components/TaskCard';
+import { useState } from 'react';
+import ProfileModal from '../components/CreateTaskModal';
 export default function Landing() {
+  const [tasks, setTasks] = useState({
+    toDo: [{ id: 1, text: 'Implement User Authentication' }],
+    underReview: [{ id: 2, text: 'Design Home Page UI' }],
+    inProgress: [{ id: 3, text: 'Integrate Cloud Storage' }],
+    finished: [{ id: 4, text: 'Conduct User Feedback Survey' }]
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleTaskUpdate = (sourceStatus, updatedSourceTasks, targetStatus, updatedTargetTasks) => {
+    setTasks((prevTasks) => ({
+      ...prevTasks,
+      [sourceStatus]: updatedSourceTasks,
+      [targetStatus]: updatedTargetTasks
+    }));
+  };
+
   return (
     <>
       <div className="home">
         <nav className="navbar">
-          <h3>Shubh Arya</h3>
+          <span className="user-header">
+            <img src={userIcon} alt="" />
+            <p className="user-name">Shubh Arya</p>
+          </span>
+
+          <div className="shortcuts">
+            <span className="shortcut-icons">
+              <img src={bellIcon} alt="" />
+              <img src={lightIcon} alt="" />
+              <img src={forwardIcon} alt="" />
+            </span>
+            <button className="logout-btn">Logout</button>
+          </div>
           <div className="links">
-            <NavLink
-              style={{
-                textDecoration: 'none'
-              }}
-              to="/"
-            >
-              {<span className="">Home</span>}
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+              {({ isActive }) => (
+                <span className={isActive ? 'active-link' : 'link'}>
+                  <img src={homeIcon} alt="" />
+                  Home
+                </span>
+              )}
             </NavLink>
-            <NavLink
-              style={{
-                textDecoration: 'none'
-              }}
-              to="/explore"
-            >
-              {<span className=""> Boards</span>}
+            <NavLink to="/boards" style={{ textDecoration: 'none' }}>
+              {({ isActive }) => (
+                <span className={isActive ? 'active-link link' : 'link'}>
+                  <img src={boardIcon} alt="" /> Boards
+                </span>
+              )}
             </NavLink>
-            <NavLink to="/bookmark">{<span className=""> Settings</span>}</NavLink>
-            <NavLink to="/profile">{<span className=""> Teams</span>}</NavLink>
-            <NavLink to="/profile">{<span className=""> Analytics</span>}</NavLink>
-            <button className="create-post-btn">Create new Task +</button>
+            <NavLink to="/settings" style={{ textDecoration: 'none' }}>
+              {({ isActive }) => (
+                <span className={isActive ? 'active-link' : 'link'}>
+                  {' '}
+                  <img src={settingIcon} alt="" />
+                  Settings
+                </span>
+              )}
+            </NavLink>
+            <NavLink to="/teams" style={{ textDecoration: 'none' }}>
+              {({ isActive }) => (
+                <span className={isActive ? 'active-link' : 'link'}>
+                  <img src={teamsIcon} alt="" /> Teams
+                </span>
+              )}
+            </NavLink>
+            <NavLink to="/analytics" style={{ textDecoration: 'none' }}>
+              {({ isActive }) => (
+                <span className={isActive ? 'active-link' : 'link'}>
+                  <img src={analyticsIcon} alt="" /> Analytics
+                </span>
+              )}
+            </NavLink>
+            <button className="create-task-btn">
+              Create new task <img src={plusIcon} alt="" />
+            </button>
           </div>
         </nav>
         <main className="main-content">
@@ -53,37 +115,23 @@ export default function Landing() {
             </div>
           </div>
           <div className="filters">
-            <input type="text" name="" id="" />
+            <input type="text" name="" id="" placeholder="Search" />
             <div className="filter-btns">
               <button>Calender View</button>
               <button>Automation</button>
               <button>Filter</button>
               <button>Share</button>
-              <button>Create new +</button>
+              <button onClick={() => setIsOpen(true)}>Create new +</button>
             </div>
           </div>
           <div className="task-manager">
-            <div>
-              <h2>To do</h2>
-              <div className="task-card"></div>
-              <button>Add new +</button>
-            </div>
-            <div>
-              <h2>In Progress</h2>
-              <div className="task-card"></div>
-              <button>Add new +</button>
-            </div>
-            <div>
-              <h2>Under Review</h2>
-              <div className="task-card"></div>
-              <button>Add new +</button>
-            </div>
-            <div>
-              <h2>Finished</h2>
-              <div className="task-card"></div>
-              <button>Add new +</button>
-            </div>
+            {['toDo', 'inProgress', 'underReview', 'finished'].map((status) => (
+              <div key={status}>
+                <TaskCard key={status} status={status} tasks={tasks} onTaskUpdate={handleTaskUpdate} />
+              </div>
+            ))}
           </div>
+          {isOpen && <ProfileModal closeModal={setIsOpen} />}
         </main>
       </div>
     </>
