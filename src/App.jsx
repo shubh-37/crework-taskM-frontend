c176ebf3f181f304cfd7012eceb76316c7b35268
Shubh Arya <shubh.arya37@gmail.com>
2024-07-31 23:55:46 +0530

    added auth for protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import LoginPage from './pages/Login';
 import SignUpPage from './pages/SignUp';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import RequiresAuth from './components/RequiresAuth';
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={
+            <RequiresAuth>
+              <LandingPage />
+            </RequiresAuth>
+          }
+        />
         <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
       <ToastContainer />
